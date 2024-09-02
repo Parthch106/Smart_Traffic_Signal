53051e409f98ffdb1a9cbef65a7b5f04522c1c04
Parthch106 <112490231+Parthch106@users.noreply.github.com>
2024-09-02 19:49:36 +0530

    updated login and register

--- smarttraffic/src/index.js
+++ smarttraffic/src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Registerpage from './frontend/register/Registerpage.jsx';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  },
+  {
+    path: "/Register",
+    element: <Registerpage/>
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
