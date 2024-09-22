3cce8d4cb63cc2d0956b1ea3044da938dfc37659
Parthch106 <112490231+Parthch106@users.noreply.github.com>
2024-09-22 20:36:31 +0530

    Update Auth.js

--- backend/Middlewares/Auth.js
+++ backend/Middlewares/Auth.js
@@ -1,14 +1,14 @@
 const jwt = require('jsonwebtoken');
 
 const ensureAuthenticated = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
+    const auth = req.headers['authorization'];
 
     if (!auth) {
         return res.status(403)
         .json({ message: 'Unauthorized, JWT token is required' });
     }
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(auth, process.env.JWT_SECRET);
         req.user = decoded;  
         next();  
     } catch (err) {
